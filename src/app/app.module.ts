5e6cb25179671acf864b553d80ad1fc0914d1d5a
flond7 <elisa.tramontidisopra@gmail.com>
2022-03-15 19:07:41 +0100

    added form module for ngmodel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppointmentRowComponent } from './components/appointment-row/appointment-row.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ViewAppointmentComponent } from './components/view-appointment/view-appointment.component';
+import { ViewAppointmentListComponent } from './components/view-appointment-list/view-appointment-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppointmentRowComponent
+    AppointmentRowComponent,
+    ViewAppointmentComponent,
+    ViewAppointmentListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
