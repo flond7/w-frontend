8e70c5125d4fa85dc4d7915909a58d97b03f93f0
flond7 <elisa.tramontidisopra@gmail.com>
2022-03-20 18:09:59 +0100

    more appointment

--- src/app/components/view-appointment/view-appointment.component.ts
+++ src/app/components/view-appointment/view-appointment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faWifi, faPersonDress, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { LIST_APPOINTMENT_TYPE, LIST_PEOPLE } from './../../constants';
 
 
 @Component({
@@ -9,21 +11,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewAppointmentComponent implements OnInit {
 
+  //font awesome icons
+  faWifi = faWifi; faPersonDress = faPersonDress; faPhone = faPhone;
+
+  //use constants list
+  peopleList: any; appointmentTypeList: any;
+
   appointmentId: any;
   appointment = {
+    nature: "person",               //options: person, web, phone
     appointmentDay: null,
-    appointmentPlace: "Pordenone",
+    appointmentPlace: "Pordenone",  //options: Pordenone, Maniago, Sacile, Spilimbergo
+    type: "Ascolto/Accoglienza",
     personOne: "",
     personTwo: "",
     volunteer: "",
     report: ""
   }
-persons = [
-  {name: "Maria Elena", surname: "Asadasdasdcaeea", mail:""},
-  { name: "Maria Elena", surname: "Asadasdasdcaeea", mail: "" },
-  { name: "Maria Elena", surname: "Asadasdasdcaeea", mail: "" },
-  { name: "Maria Elena", surname: "Asadasdasdcaeea", mail: "" }
-]
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -31,6 +36,9 @@ persons = [
       console.log(params); // { orderby: "price" }
       this.appointmentId = params['id'];
     });
+
+    this.peopleList = LIST_PEOPLE;
+    this.appointmentTypeList = LIST_APPOINTMENT_TYPE;
   }
 
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,24 @@
+export const LIST_PEOPLE: any = [
+    { name: "Elisa", surname: "P", mail: "ep@gmail.com" },
+    { name: "Laura", surname: "B", mail: "lb@gmail.com" },
+    { name: "Maria Elena", surname: "ASDASDASDASDADSASDASD", mail: "mea@gmail.com" },
+    { name: "Anna", surname: "C", mail: "ac@gmail.com" }
+  ]
+export const LIST_APPOINTMENT_TYPE: any = [
+  "Ascolto/Accoglienza", "Legale", "In casa rifugio"
+]
+
+export const LIST_SERIVICE: any = [
+  { name: "Pronto intervento/Messa in sicurezza", id: 1 },
+  { name: "Ascolto", id: 2 },
+  { name: "Accoglienza", id: 4 },
+  { name: "Orientamento ai servizi della rete", id: 5 },
+  { name: "Consulenza psicologica", id: 6 },
+
+
+  { name: "Pronto intervento/Messa in sicurezza", id: 1 },
+  { name: "Ascolto", id: 2 },
+  { name: "Accoglienza", id: 4 },
+  { name: "Orientamento ai servizi della rete", id: 5 },
+  { name: "Consulenza psicologica", id: 6 },
+]
