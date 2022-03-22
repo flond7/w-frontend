6b6bdb968ae4747c602754d623eee7e0d96553b3
flond7 <elisa.tramontidisopra@gmail.com>
2022-03-22 16:54:20 +0100

    added service list to constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -14,11 +14,14 @@ export const LIST_SERIVICE: any = [
   { name: "Accoglienza", id: 4 },
   { name: "Orientamento ai servizi della rete", id: 5 },
   { name: "Consulenza psicologica", id: 6 },
-
-
-  { name: "Pronto intervento/Messa in sicurezza", id: 1 },
-  { name: "Ascolto", id: 2 },
-  { name: "Accoglienza", id: 4 },
-  { name: "Orientamento ai servizi della rete", id: 5 },
-  { name: "Consulenza psicologica", id: 6 },
+  { name: "Consulenza legale/giudiziaria", id: 7 },
+  { name: "Supporto/consulenza alloggiativa", id: 8 },
+  { name: "Sostegno all'autonomia", id: 9 },
+  { name: "Orientamento lavorativo", id: 10 },
+  { name: "Percorso di allontanamento", id: 11 },
+  { name: "Supporto figli minorenni", id: 12 },
+  { name: "Sostegno alla genitorialitá", id: 13},
+  { name: "Mediazione linguistica", id: 14 },
+  { name: "Altre richieste donne straniere", id: 15 },
+  { name: "Altro", id: 16 }
 ]
